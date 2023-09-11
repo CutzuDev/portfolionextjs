786f7a5067ab716117f399c576d5ca72673acc50
CutzuDev <alexfarkas05@gmail.com>
2023-09-11 17:28:44 +0300

    updt

--- components/ui/navigation/Navbar.tsx
+++ components/ui/navigation/Navbar.tsx
@@ -1,10 +1,9 @@
-
 import { NavImage, NavLinks } from "./NavBarComps";
 
 function Navbar() {
   return (
     <div className="fixed z-50 flex w-full items-center justify-center  backdrop-blur-md">
-      <div className="relative flex w-full max-w-[1200px] items-center py-[15px] justify-between px-5">
+      <div className="relative flex w-full max-w-[1200px] items-center justify-between px-5 py-[15px]">
         <NavImage />
         <NavLinks />
         <div className="absolute bottom-0 left-1/2 h-[1px] w-[80%] -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-200/10 to-transparent" />
@@ -12,5 +11,7 @@ function Navbar() {
     </div>
   );
 }
+
+
 
 export default Navbar;
