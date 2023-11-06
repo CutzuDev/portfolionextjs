28505e1933a8b0badf0cbcff6469c384a16e1c5e
CutzuDev <alexfarkas05@gmail.com>
2023-11-06 21:00:06 +0200

    updt exchange

--- app/exchange/page.tsx
+++ app/exchange/page.tsx
@@ -10,6 +10,13 @@ export default function Home() {
   const [valueEur, setvalueEur] = useState<any>();
   const [calculatedRon, setcalculatedRon] = useState<any>(0);
   const [calculatedEur, setcalculatedEur] = useState<any>(0);
+
+  const [randomNumber, setrandomNumber] = useState<string>();
+  useEffect(() => {
+    const number = Math.floor(Math.random() * 1e15);
+    setrandomNumber(`0${number.toFixed(0)}`);
+  }, []);
+
   useEffect(() => {
     if (!Number.isNaN(+valueRon) && +valueRon !== 0) {
       +valueRon > 25
@@ -38,13 +45,68 @@ export default function Home() {
       >
         <Divider />
         <div className="flex flex-col items-start justify-center gap-2.5">
+          <div className="mb-5 flex w-full flex-col items-start justify-center gap-2.5">
+            <motion.h1
+              className="mb-5 w-full bg-gradient-to-t from-neutral-400 to-white bg-clip-text text-center text-2xl font-bold text-transparent md:text-4xl"
+              variants={childrenVar("vertical", "spring")}
+            >
+              INFO EXCHANGE
+            </motion.h1>
+            <motion.span
+              className="bg-gradient-to-t from-neutral-400 to-white bg-clip-text text-xl font-semibold text-transparent"
+              variants={childrenVar("vertical", "spring")}
+            >
+              Taxa:
+            </motion.span>
+            <motion.span
+              className="bg-gradient-to-t from-neutral-400 to-white bg-clip-text font-semibold text-transparent"
+              variants={childrenVar("vertical", "spring")}
+            >
+              {`Sub 10€ -> 30% , Peste 10€ -> 20%`}
+            </motion.span>
+            <motion.span
+              className="bg-gradient-to-t from-neutral-400 to-white bg-clip-text text-xl font-semibold text-transparent"
+              variants={childrenVar("vertical", "spring")}
+            >
+              Pasi Exchange:
+            </motion.span>
+            <motion.ul
+              className="flex flex-col items-start justify-center"
+              variants={childrenVar("vertical", "spring")}
+            >
+              <motion.li className="flex flex-col items-start justify-center">
+                <span className="bg-gradient-to-t  from-neutral-400 to-white bg-clip-text font-semibold text-transparent">
+                  1. Sub formatul:
+                </span>
+                <div className="my-2.5 flex flex-col items-start justify-center gap-1 rounded-md border-2 border-[#383A40] bg-[#313338] px-5 py-2.5">
+                  <span>emailultau123@gmail.com</span>
+                  <span>{randomNumber}</span>
+                </div>
+              </motion.li>
+
+              <motion.li className="flex flex-col items-start justify-center gap-0 md:flex-row md:items-center md:gap-2.5">
+                <span className="bg-gradient-to-t from-neutral-400 to-white bg-clip-text font-semibold text-transparent">
+                  2. Trimite Email Paypal
+                </span>
+                <span className="font-normal text-red-500">
+                  (NU nume paypal / paypal.me)
+                </span>
+              </motion.li>
+              <motion.li className="bg-gradient-to-t from-neutral-400 to-white bg-clip-text font-semibold text-transparent">
+                3. Trimite Cod Paysafe
+              </motion.li>
+            </motion.ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-start justify-center gap-2.5">
           <motion.span
             variants={childrenVar("vertical", "spring")}
             className="bg-gradient-to-t from-neutral-400 to-white bg-clip-text font-semibold text-transparent"
           >
             RON
           </motion.span>
-          <div className="flex items-end justify-center gap-10">
+          <div className="flex flex-col items-start justify-center gap-10 md:flex-row md:items-end">
             <div className="flex flex-col items-start justify-center gap-2.5">
               <motion.span
                 variants={childrenVar("vertical", "spring")}
@@ -89,7 +151,7 @@ export default function Home() {
           >
             EUR
           </motion.span>
-          <div className="flex items-end justify-center gap-10">
+          <div className="flex flex-col items-start justify-center gap-10 md:flex-row md:items-end">
             <div className="flex flex-col items-start justify-center gap-2.5">
               <motion.span
                 variants={childrenVar("vertical", "spring")}
