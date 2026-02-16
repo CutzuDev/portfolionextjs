00bec2f29322ffdc96b47cf500e8be9661e6ba7c
CutzuDev <alexfarkas05@gmail.com>
2026-02-16 09:58:12 +0200

    style: Add `text-white` class to the body element.

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} text-white`}>
         <Analytics />
         <SpeedInsights />
         <Navbar />
